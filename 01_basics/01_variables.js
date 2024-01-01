4317710cb930be0a367f093cf594a46882be51f8
SystemByZ <150690593+SystemByZ@users.noreply.github.com>
2024-01-01 18:02:47 +0000

    var config

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,11 @@ const accountId = 2111028140
 let accountEmail = "mh450298@gmail.com"
 var accountPassword = "6251"
 accountCity = "cumilla"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity ])
+console.log(accountPassword);
+console.log(accountCity);
+
+console.log(accountEmail);
